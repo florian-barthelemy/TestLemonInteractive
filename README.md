718e6397bbf668d40f4d74f34f6ba16cb43f936e
florian-barthelemy <florian-barthelemy@hotmail.com>
2024-01-21 12:24:09 +0100

    feat : ajout de la partie fond vert

--- README.md
+++ README.md
@@ -8,14 +8,19 @@ Ajout d'un gap pour espacer mes éléments dans la partie liens de navigation.
 Pour le côté responsive, j'ai décidé d'uiliser la fonctionnalité display: none ou display : block pour cacher ou afficher des éléments selon le rendu de l'affichage que l'on souhaite.
 
 
-choix de conception pour la rubrique news :
+Choix de conception pour la rubrique news :
 Récupération des différentes photos sur la maquette Figma.
 Réduction de la taille des images pour que cela ne paraisse pas trop grand.
 Utilisation de position absolu et relative pour gérer l'emplacement des différentes images.
 Pour le côté responsive, au lieu d'afficher les éléments en colone, je les affiches en ligne.
 
-choix de conception pour les multicards :
+Choix de conception pour les multicards :
 Récupération des différentes photos, tailles sur la maquette Figma.
 Utilisation de position absolu et relative pour gérer l'emplacement des différentes images.
 Utilisation de flexbox pour aligner les 4 cartes et les disposer.
 Pour le responsive j'ai gardé la même taille de carte mais j'ai mis un scrolling pour ne pas dépasser de l'écran.
+
+Choix de conception pour la rubrique avec fond vert :
+Récupération des différentes photos, tailles sur la maquette Figma.
+Utilisation de position absolu et relative pour gérer l'emplacement des différentes images.
+Pour le responsive j'ai changé la taille du fond vert ainsi que les images et je suis passé sur un alignement en colone.
--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-const hamburger  = document.querySelector("#hamburger");
+const hamburger = document.querySelector("#hamburger");
 const iconSearchResponsive = document.querySelector(".icon-search-responsive");
 const navbar = document.querySelector(".navbar");
-const navButtons =  document.querySelectorAll(".nav-btn button");
+const navButtons = document.querySelectorAll(".nav-btn button");
 const navLinks = document.querySelectorAll(".nav-link");
 const news = document.querySelector(".news");
 const multicards = document.querySelector(".multicards");
+const greenBackgroundContainer = document.querySelector(".green-background-container");
 
 /**
  * permet d'afficher sur mobile le contenu du menu burger quand on clique sur ce dernier
  */
-hamburger.addEventListener("click" , ()=>{
+hamburger.addEventListener("click", () => {
     navbar.classList.toggle("active");
     news.style.display = "none";
     multicards.style.display = "none";
+    greenBackgroundContainer.style.display = "none";
 });
 
 /**
  * permet de cacher le contenu du menu burger quand on clique sur l'icone loupe de la barre de recherche
  */
-iconSearchResponsive.addEventListener("click",()=>{
+iconSearchResponsive.addEventListener("click", () => {
     navbar.classList.remove("active");
     news.style.display = "block";
     multicards.style.display = "flex";
+    greenBackgroundContainer.style.display = "flex";
 });
 
 /**
  * permet de cacher le contenu du menu burger quand on clique sur le bouton register ou login
  */
-navButtons.forEach(navButton =>{
-    navButton.addEventListener("click",()=>{
+navButtons.forEach(navButton => {
+    navButton.addEventListener("click", () => {
         navbar.classList.remove("active");
         news.style.display = "block";
         multicards.style.display = "flex";
+        greenBackgroundContainer.style.display = "flex";
     });
 });
 
@@ -39,11 +43,12 @@ navButtons.forEach(navButton =>{
 /**
  * permet de cacher le contenu du menu burger quand on clique sur l'un des éléments de navigation sur notre site
  */
-navLinks.forEach(navLink =>{
-    navLink.addEventListener("click",()=>{
+navLinks.forEach(navLink => {
+    navLink.addEventListener("click", () => {
         navbar.classList.remove("active");
         news.style.display = "block";
         multicards.style.display = "flex";
+        greenBackgroundContainer.style.display = "flex";
     });
 });
 
